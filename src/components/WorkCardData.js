b898e205b955c11688c550331f7dcb7f90ad7faa
Md Arwaz <mdarwaznew2017@gmail.com>
2023-03-17 19:54:41 +0530

    change in view and source link

--- src/components/WorkCardData.js
+++ src/components/WorkCardData.js
@@ -10,48 +10,48 @@ const ProjectCardData = [
         imgsrc: pro1,
         title: 'Portfolio Website',
         text: "A portfolio website is a unique way to showcase your work and let others know about yourself. It's like an evergreen platform for your projects, case studies, and information about you. it is one of the best ways to express yourself.",
-        view: 'https://www.google.com',
-        source: 'https://www.google.com',
+        view: 'https://my-portfolio-mdarwaz.web.app/',
+        source: 'https://github.com/mdarwaz0786/my-portfolio',
     },
 
     {
         imgsrc: pro2,
         title: 'Ecommerce Website',
         text: "A website that allows people to buy and sell physical goods, services, and digital products over the internet rather than at a brick-and-mortar location. Through an e-commerce website, a business can process orders.",
-        view: 'https://www.google.com',
-        source: 'https://www.google.com',
+        view: 'https://ecommerce-app-mdarwaz.web.app/',
+        source: 'https://github.com/mdarwaz0786/ecommerce-app',
     },
 
     {
         imgsrc: pro3,
         title: 'Resturant Website',
         text: "A restaurant is a business that prepares and serves food and drinks to customers. Meals are generally served and eaten on the premises, restaurants also offer take-out and food delivery services.",
-        view: 'https://www.google.com',
-        source: 'https://www.google.com',
+        view: 'https://resturant-app-mdarwaz.web.app/',
+        source: 'https://github.com/mdarwaz0786/resturant-app',
     },
 
     {
         imgsrc: pro4,
         title: 'Weather App',
         text: "Our Weather Forecast App Development enables the user to add numerous locations to the list to verify the weather data accordingly. The user will be able to view the updated weather data every hour for any given location. ",
-        view: 'https://www.google.com',
-        source: 'https://www.google.com',
+        view: '//weather-app-mdarwaz.web.app/',
+        source: 'https://github.com/mdarwaz0786/weather-app',
     },
 
     {
         imgsrc: pro5,
         title: 'Myntra Clone',
         text: "Myntra is a one stop shop for all your fashion and lifestyle needs. Being India's largest e-commerce store for fashion and lifestyle products, Myntra aims at providing a hassle free and enjoyable shopping experience to shoppers across the country with the widest range of brands and products on its portal.",
-        view: 'https://www.google.com',
-        source: 'https://www.google.com',
+        view: 'https://myntra-clone-mdarwaz.web.app/',
+        source: 'https://github.com/mdarwaz0786/myntra-clone',
     },
 
     {
         imgsrc: pro6,
         title: 'Developer Landing Page',
         text: "A landing page is a standalone web page that a person 'lands' on after clicking through from an email, ad, or other digital location. Every content strategy should include the use of landing pages to convert more traffic and increase conversions.",
-        view: 'https://www.google.com',
-        source: 'https://www.google.com',
+        view: 'https://myntra-clone-mdarwaz.web.app/',
+        source: 'https://github.com/mdarwaz0786/landing-page',
     },
 ];
 
